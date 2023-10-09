3a0afc5a1f68bd0bc4f05a477691c8d505c90f30
kurbisz <krzysiu_urbisz@interia.pl>
2023-10-10 00:26:01 +0200

    removed time counters

--- src/main/java/com/kurbisz/algorithm/AlphaBeta.java
+++ src/main/java/com/kurbisz/algorithm/AlphaBeta.java
@@ -22,19 +22,19 @@ public class AlphaBeta extends Algorithm {
     }
 
     MyPair minMax(Move m, int actDepth, int alpha, int beta, boolean isMax) {
-        long begin = System.nanoTime();
+//        long begin = System.nanoTime();
         int endAdvantage = heuristic.getEndAdvantage(m.field);
-        synchronized (STATISTICS_END_TIME_OBJ) {
-            STATISTICS_END_TIME += System.nanoTime() - begin;
-        }
+//        synchronized (STATISTICS_END_TIME_OBJ) {
+//            STATISTICS_END_TIME += System.nanoTime() - begin;
+//        }
         if (endAdvantage != 0) return new MyPair(m, endAdvantage, actDepth);
 
         if (actDepth == depth) {
-            long begin1 = System.nanoTime();
+//            long begin1 = System.nanoTime();
             int evaluation = heuristic.getEvaluation(m.field);
-            synchronized (STATISTICS_EVAL_TIME_OBJ) {
-                STATISTICS_EVAL_TIME += System.nanoTime() - begin1;
-            }
+//            synchronized (STATISTICS_EVAL_TIME_OBJ) {
+//                STATISTICS_EVAL_TIME += System.nanoTime() - begin1;
+//            }
             return new MyPair(m, evaluation);
         }
         int player = isMax ? playerNumber : 3 - playerNumber;
--- src/main/java/com/kurbisz/player/BotPlayer.java
+++ src/main/java/com/kurbisz/player/BotPlayer.java
@@ -27,11 +27,11 @@ public class BotPlayer extends Player {
         if (logs) b.printBoard();
         Algorithm a = new AlphaBeta(b.n, playerNumber, depth, heuristic);
 
-        long begin = System.nanoTime();
+//        long begin = System.nanoTime();
         int minMaxMove = a.getMove(b.fields);
-        synchronized (STATISTICS_OVERALL_TIME_OBJ) {
-            STATISTICS_OVERALL_TIME += System.nanoTime() - begin;
-        }
+//        synchronized (STATISTICS_OVERALL_TIME_OBJ) {
+//            STATISTICS_OVERALL_TIME += System.nanoTime() - begin;
+//        }
         return minMaxMove;
     }
 
